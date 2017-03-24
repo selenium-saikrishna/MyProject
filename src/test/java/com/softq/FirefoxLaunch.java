ea71b955fa8c13a6b45c69faf7aabe25d89524fd
Sai <Sai@Sai-PC>
2017-03-23 22:00:23 -0700

    First Commit

---
+++ src/test/java/com/softq/FirefoxLaunch.java
@@ -0,0 +1,13 @@
+package com.softq;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class FirefoxLaunch {
+	@Test
+	public void launchTest()
+	{
+		FirefoxDriver driver=new FirefoxDriver();
+	}
+
+}
